2d3398bae67e2afa4c0d29a4296f7da3092f2764
Yang Yang <kiooss@gmail.com>
2021-01-05 18:36:22 +0900

    feat: get-weather.

--- bin/get-weather.js
+++ bin/get-weather.js
@@ -13,33 +13,43 @@ function icon(weather) {
   switch (weather.icon) {
     case "clear-day":
       // TODO: add sunrise/sunset 🌇 🌅
-      return "☀️";
+      // return "☀️";
+      return "\uf185";
     case "clear-night":
-      return "🌙";
+      // return "🌙";
+      return "\uf186";
     case "sleet":
-      return "🌨";
+      // return "🌨";
+      return "\ufe3ad";
     case "rain":
-      return "☔";
+      // return "☔";
+      return "\ue318";
     case "snow":
-      return "❄️";
+      // return "❄️";
+      return "\uf2dc";
     case "wind":
-      return "💨";
+      // return "💨";
+      return "\ue27e";
     case "fog":
-      return "🌁";
+      // return "🌁";
+      return "fa90";
     case "cloudy":
-      return "☁️";
+      // return "☁️";
+      return "\ue312";
     case "partly-cloudy-night":
-      return "☁️🌛";
+      // return "☁️🌛";
+      return "\ue319";
     case "partly-cloudy-day":
-      return "⛅️";
+      // return "⛅️";
+      return "\ue376";
     default:
       return weather.icon;
   }
 }
 
 function temperature(weather) {
-  const tempC = weather.temperature;
-  const apparentTemperature = weather.apparentTemperature;
+  const tempC = Math.round(weather.temperature * 10) / 10;
+  const apparentTemperature = Math.round(weather.apparentTemperature * 10) / 10;
   const tempF = tempC * 1.8 + 32;
 
   let color = 255;
@@ -59,16 +69,18 @@ function temperature(weather) {
     color = 196;
   }
 
-  let face = "☺️";
+  let face = "\uf118";
   if (apparentTemperature > 40) {
-    face = "🥵";
+    face = "\ue3bf";
   } else if (apparentTemperature > 30) {
-    face = "😳";
+    face = "\ue36b";
+  } else if (apparentTemperature < 10) {
+    face = "\ue36f";
   } else if (apparentTemperature < 0) {
-    face = "🥶";
+    face = "\uf9b6";
   }
   // return `#[fg=colour${color}]${parseInt((temp - 32) / 1.8)}°C`
-  return `#[fg=colour${color}]${tempC}°C ${face}  ${apparentTemperature}°C`;
+  return `#[fg=colour${color}]${tempC}\ue339 #[fg=colour00]${face} #[fg=colour${color}]${apparentTemperature}\ue339`;
 }
 
 const latlon = {
@@ -80,7 +92,7 @@ const apiUrl = `https://api.darksky.net/forecast/${forecastIOApiKey}/${latlon.la
 
 getWeather(apiUrl)
   .then(res => {
-    console.log(`${icon(res.currently)}  ${temperature(res.currently)}`);
+    console.log(`${icon(res.currently)} ${temperature(res.currently)}`);
   })
   .catch(err => {
     lib.handleError("get-weather", err);
